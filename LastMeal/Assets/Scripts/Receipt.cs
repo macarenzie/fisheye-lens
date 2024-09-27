b1d4e92be3284c48048775925b55d04e2d01fde8
nm8011 <nm8011@rit.edu>
2024-09-27 19:13:32 -0400

    simple receipt finished

--- LastMeal/Assets/Scripts/Receipt.cs
+++ LastMeal/Assets/Scripts/Receipt.cs
@@ -1,31 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class Receipt : MonoBehaviour
 {
-    public Text foodChoice;
-    public Text ingredients;
+    [SerializeField] TMP_Text foodChoice;
+    [SerializeField] TMP_Text ingredients;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+        ingredients.text = null;
+        if (foodChoice.text.Trim().ToLower() == "sandwich")
+        {
+            //print out ingredients of sandwich
+            string tempString = "Bacon, Lettuce, Tomatoe"; //will add txt file
+            string[] listOfIngredients = tempString.Split(',');
+            foreach (string ingredient in listOfIngredients)
+            {
+                ingredients.text += $"  * {ingredient}\n";
+            }
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(foodChoice.text == "Sandwich".Trim().ToLower())
-        {
-            //print out ingredients of sandwich
-            string tempString = "Bacon, Lettuce, Tomatoe"; //will add txt file
-            string[] listOfIngredients = tempString.Split(',');
-            foreach(string ingredient in listOfIngredients)
-            {
-                ingredients.text += $"  * {ingredient}\n";
-            }
-            
-        }
+        
     }
 }
