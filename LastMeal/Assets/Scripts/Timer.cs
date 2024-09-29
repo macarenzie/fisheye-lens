69fefa886ccdadb486b04fedeb29425e741bf332
macarenzie <mhl8656@rit.edu>
2024-09-29 19:40:23 -0400

    Timer function complete
    Timer displayed on screen in a { minutes : seconds } format.
    
    Three buttons affect the timer:
    - start timer: starts the timer with the timeRemaining value
    - send order: simulates completing an order. changes the display text to say "order complete" but timeRemaining stays the same
    - reset timer: stops and resets the timer to the initial value indicated in the editor

--- LastMeal/Assets/Scripts/Timer.cs
+++ LastMeal/Assets/Scripts/Timer.cs
@@ -3,36 +3,62 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 /// <summary>
-/// Class: Timer
-/// Purpose: handles the logic behind the countdown timer for the cooking portion of the game
+/// Script: Timer
+/// Purpose: handles the logic behind the countdown timer for the 
+///          cooking portion of the game
 /// Author(s): McKenzie Lam
 /// </summary>
 public class Timer : MonoBehaviour
 {
-    // fields
-    public float timeRemaining = 10;
-    public bool timerIsRunning = false;
-    public TMP_Text timerText;
+    // FIELDS -----------------------------------------------------------------
     
+    // timer logic
+    [SerializeField] protected float initialTime;
+    private float timeRemaining;
+    protected bool timerIsRunning;
+    
+    // ui
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button sendOrderButton;
+    [SerializeField] private Button resetButton;
+    [SerializeField] protected TMP_Text timerText;
+
+
     void Start()
     {
-        timerIsRunning = true;
+        // set up timer
+        timerIsRunning = false;
+        timeRemaining = initialTime;
+
+        // hook up the buttons to their methods
+        startButton.onClick.AddListener(startTimer);
+        sendOrderButton.onClick.AddListener(sendOrder);
+        resetButton.onClick.AddListener(resetTimer);
+
+        // print timer on screen
+        displayTime(initialTime);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // if startTimer button clicked
         if (timerIsRunning)
         {
+            // start the timer if it hasn't finished yet
             if (timeRemaining > 0)
             {
                 timeRemaining -= Time.deltaTime;
+                displayTime(timeRemaining);
             }
+            
+            // determine if the timer ran out
             else
             {
                 Debug.Log("Order failed");
+                timerText.text = "Order failed";
                 timeRemaining = 0;
                 timerIsRunning = false;
             }
@@ -40,7 +66,8 @@ public class Timer : MonoBehaviour
         
     }
 
-    void DisplayTime(float timeToDisplay)
+    // METHODS ----------------------------------------------------------------
+    void displayTime(float timeToDisplay)
     {
         timeToDisplay += 1;
 
@@ -48,5 +75,28 @@ public class Timer : MonoBehaviour
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void startTimer()
+    {
+        timerIsRunning = true;
+    }
+
+    void sendOrder()
+    {
+        timerIsRunning = false;
+        timerText.text = "Order complete";
+    }
+
+    void resetTimer()
+    {
+        // stop the timer from running
+        timerIsRunning = false;
+
+        // reset the time
+        timeRemaining = initialTime;
+
+        // update display
+        displayTime(timeRemaining);
     }
 }
