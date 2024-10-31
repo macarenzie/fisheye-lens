c80fa6e7eda59bfe0072b5fc7a4a3ff8a90918c2
YedTM <rcc3452@rit.edu>
2024-10-31 17:10:13 -0400

    Revert "something"
    This reverts commit a33a2979fa5b5c2bfda2a0be47d9241e7d283b8b.

--- LastMeal/Assets/Scripts/Cooking/CreateObject.cs
+++ LastMeal/Assets/Scripts/Cooking/CreateObject.cs
@@ -5,12 +5,13 @@ using UnityEngine.InputSystem;
 
 public class CreateObject : MonoBehaviour
 {
-    public Drag sprite;
+    public SpriteInfo spriteInfo;
+    public GameObject ingrediant;
     public IngrediantManager manager;
     public GameObject spawner;
 
     // Creates a given ingrediant at a given spawn location onClick
-    public void SpawnIngrediantSprite()
+    public void SpawnIngrediantSprite(Drag sprite)
     {
         manager.AddSprite(sprite, new Vector3(spawner.transform.position.x, 
             spawner.transform.position.y, spawner.transform.position.z - 1));
